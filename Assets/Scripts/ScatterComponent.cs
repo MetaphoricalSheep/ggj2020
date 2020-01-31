c15c73baeb00b96ba3cf7252b912dcb0c2a12a15
Rene Rathmann <rene.klilian.rathmann@gmail.com>
2020-01-31 23:39:10 +0100

    changes

--- Assets/Scripts/ScatterComponent.cs
+++ Assets/Scripts/ScatterComponent.cs
@@ -3,17 +3,26 @@ using UnityEngine;
 public class ScatterComponent : MonoBehaviour
 {
     [SerializeField] private Vector2 _range = new Vector2(0, 5);
+    [SerializeField] private Vector2 _rangeY = new Vector2(0, 5);
+    [SerializeField] private Vector2 _rotation = new Vector2(0, 90);
 
     private void Awake()
     {
         var rigidBody = GetComponent<Rigidbody>();
         rigidBody.velocity = RandomVector();
+        rigidBody.rotation = RandomQuaternion();
+    }
+
+    private Quaternion RandomQuaternion() {
+        return Quaternion.Euler(Random.Range(_rotation.x, _rotation.y),
+            Random.Range(_rotation.x, _rotation.y),
+            Random.Range(_rotation.x, _rotation.y));
     }
 
     private Vector3 RandomVector()
     {
         var x = Random.Range(_range.x, _range.y);
-        var y = Random.Range(_range.x, _range.y);
+        var y = Random.Range(_rangeY.x, _rangeY.y);
         var z = Random.Range(_range.x, _range.y);
 
         return new Vector3(x, y, z);
