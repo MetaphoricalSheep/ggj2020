2c55d5c086ced37a5157a51b4dfae563dfb2f58e
Rene Rathmann <rene.klilian.rathmann@gmail.com>
2020-01-31 21:56:40 +0100

    lights

---
+++ Assets/Scripts/Health/CharacterHealth.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : CharacterBar {
+    public bool isSafe = true;
+    public float damagePerSecond = 0.0f;
+
+    protected override void Update() {
+        base.Update();
+        if (isSafe) {
+            base.Add(maxAmount);
+        } else {
+            base.Remove(damagePerSecond * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Light/LightManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager: MonoBehaviour {
+    private static List<Light> lights;
+    private static GameObject player;
+
+    public static void AddLight(Light light) {
+        lights.Add(light);
+    }
+
+    public static List<Light> GetLights() {
+        return lights;
+    }
+
+    private void Start() {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update() {
+        if (!PlayerIsSafe()) {
+            // player
+        }
+    }
+
+    private bool PlayerIsSafe() {
+        foreach (Light light in lights) {
+            if (Vector3.Distance(light.transform.position, player.transform.position) < light.range) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
