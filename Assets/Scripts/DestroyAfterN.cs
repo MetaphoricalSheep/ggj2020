cd9c8f22c39768417e307704c9901d1560765134
Rene Rathmann <rene.klilian.rathmann@gmail.com>
2020-02-01 22:21:35 +0100

    Changes

---
+++ Assets/Scripts/DestroyAfterN.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DestroyAfterN : MonoBehaviour {
+    public float seconds;
+
+    void Start() {
+        Destroy(gameObject, seconds);
+    }
+}
---
+++ Assets/Scripts/SpawnLogText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLogText : MonoBehaviour {
+    public GameObject returnText;
+    private bool isQuitting;
+
+    void OnApplicationQuit() {
+        isQuitting = true;
+    }
+
+    private void OnDestroy() {
+        if (isQuitting) {
+            return;
+        }
+        GameObject tt = Instantiate(returnText);
+        tt.transform.position = this.transform.position;
+        Destroy(tt, 5f);
+    }
+}
