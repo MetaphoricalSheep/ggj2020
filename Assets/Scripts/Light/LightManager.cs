5229cd8e64bb2ccd51dded5684a35a7b6e5a6ee9
Rene Rathmann <rene.klilian.rathmann@gmail.com>
2020-02-01 17:21:53 +0100

    Added HDRP

--- Assets/Scripts/Light/LightManager.cs
+++ Assets/Scripts/Light/LightManager.cs
@@ -1,46 +1,50 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class LightManager : MonoBehaviour
-{
+public class LightManager : MonoBehaviour {
     private static List<Light> lights = new List<Light>();
     private static GameObject player;
     private static CharacterHealth characterHealth;
-    
+    public static float disableDistance = 15f;
+
     public static List<Light> GetLights() => lights;
 
-    public static void AddLight(Light light)
-    {
+    public static void AddLight(Light light) {
         lights.Add(light);
     }
 
-    public static void RemoveLight(Light light)
-    {
+    public static void RemoveLight(Light light) {
         lights.Remove(light);
     }
 
-    private void Start()
-    {
+    private void Start() {
         player = GameObject.FindWithTag("Player");
         characterHealth = player.GetComponent<CharacterHealth>();
     }
 
-    private void Update()
-    {
+    private void Update() {
+        DeactivateLights();
         characterHealth.isSafe = PlayerIsSafe();
     }
 
-    private static bool PlayerIsSafe() 
-    {
-        
-        foreach (var light in lights) 
-        {            
-            if (Vector3.Distance(light.transform.position, player.transform.position) < light.range) 
-            {
+    private static void DeactivateLights() {
+        foreach (var light in lights) {
+            float distance = Vector3.Distance(light.transform.position, player.transform.position);
+            light.enabled = distance < light.range + disableDistance;
+        }
+    }
+
+    private static bool PlayerIsSafe() {
+
+        foreach (var light in lights) {
+            float distance = Vector3.Distance(light.transform.position, player.transform.position);
+
+
+            if (light.enabled && distance < light.range) {
                 return true;
             }
         }
-        
+
         return false;
     }
 }
